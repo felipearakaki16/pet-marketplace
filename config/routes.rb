237338c9ac48811885d6f1596ec4d6ad5c1a3e05
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-02 16:29:04 -0300

    Corrections on resources of routes (changed to: products and orders).

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :product
-  resources :order, only: %i( index show create destroy)
+  resources :products
+  resources :orders, only: %i( index show create destroy)
 end
