f0a291091f5d1e6e392630892174c6ecb7885a0b
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-02 11:14:29 -0300

    Model's associations and validations.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  belongs_to :user
+  has_many :orders, dependence: :destroy
+
+  validates :name,  presence: true, uniqueness: true
+  validates :price,  presence: true
+  validates :category
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :orders, dependece: :destroy
+  has_many :products, dependece: :destroy
+
+  validates :user_name,  presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }, unless: "password.nil?"
+  validates :store_name, presence: true, uniqueness: true
 end
