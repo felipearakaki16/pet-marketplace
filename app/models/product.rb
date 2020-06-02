093fd4d98ca32a14599728ed3f44809893e9d1bb
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-02 16:58:52 -0300

    Product model. Validates user presence true.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :orders, dependent: :destroy
 
+  validates :user, presence: true
   validates :name,  presence: true, uniqueness: true
   validates :price,  presence: true
 end
