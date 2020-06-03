d090a068a8863219a133d6f8dfe4546086d188d7
Isabela Porto <isamporto@hotmail.com>
2020-06-03 12:21:00 -0300

    Added the acts as taggable gem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many :orders, dependent: :destroy
+  acts_as_taggable_on :tags
 
   validates :user, presence: true
   validates :name,  presence: true, uniqueness: true
