bc654869379de9108a9b4ec0a3ff680153956b74
Isabela Porto <isamporto@hotmail.com>
2020-06-03 23:57:46 -0300

    Tried to add owlcarousel, but didn't wor

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,9 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
+//= require jquery_ujs
+//= require owl.carousel
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -31,4 +34,5 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  $(".owl-carousel").owlCarousel();
 });
