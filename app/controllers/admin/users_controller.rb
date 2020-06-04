046693263637d47be56e1535308547bec9fa60dc
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-04 12:09:42 -0300

    Namespacing deleted. New route Pages/show_user

--- app/controllers/admin/users_controller.rb
+++
@@ -1,7 +0,0 @@
-class Admin::UsersController < ActionController::Base
-    
-  def show
-    user = User.find(params[:id])
-    @products = Product.where(user_id: user.id)  
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
 
   get "/components", to: "pages#components"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :admin do 
-    resources :users, only: [ :show ]
-  end
   resources :products, except: :destroy
   resources :orders, only: %i(index show create update)
+
+  get '/user_products', to: 'pages#show_user'
 end
