41b6dce1b9a963503b150145ba6f6585173735e3
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-04 19:17:36 -0300

    Fixed seed code.

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ puts 'Creating 10 fake products...'
     price: rand(1..99),
     description: "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, c",
     user_id: rand(1..30),
-    brand:,
+    brand: ['Whiskas', 'Pedigree', 'Sunrise', 'Eternal', 'GoodFoods', 'Alpiste', 'Sao Lourenço', 'Itajaí', 'Alpargatos'],
     tag_list: ['Dogs', 'Cats', 'Fishes', 'Birds'].sample
   )
   product.save!
