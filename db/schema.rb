4122c47787c03bef5a2d9915c8c851cae6929e70
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-04 16:25:15 -0300

    User model name added. Avatar photo added.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_231213) do
+ActiveRecord::Schema.define(version: 2020_06_04_180914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,20 +36,6 @@ ActiveRecord::Schema.define(version: 2020_06_03_231213) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "animals", force: :cascade do |t|
-    t.string "pet"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "animal_id", null: false
-    t.index ["animal_id"], name: "index_categories_on_animal_id"
-  end
-
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "product_id", null: false
@@ -63,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_231213) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
+    t.string "category"
     t.float "price"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -106,12 +93,12 @@ ActiveRecord::Schema.define(version: 2020_06_03_231213) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "categories", "animals"
   add_foreign_key "orders", "products"
   add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
