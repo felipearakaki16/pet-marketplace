4196b4edad4e5a0b5b5d17bd01bb9cad13ffd1c2
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-06-05 11:58:42 -0300

    Fixed login to visitors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,7 @@ class ProductsController < ApplicationController
   end
 
   def show
+
     @order = Order.new
     @order.product = @product
   end
@@ -17,6 +18,7 @@ class ProductsController < ApplicationController
   end
 
   def create
+    
     @product = Product.new(product_params)
     @product.user = current_user
     if @product.save
